f0ebc67ed11f47872b4e5268cf6eb3e01fe15082
PsyhoBelka <agroghkov@gmail.com>
2018-08-03 21:16:18 +0300

    Add OOP Tests complete

--- src/ua/rozhkov/Main.java
+++ src/ua/rozhkov/Main.java
@@ -7,27 +7,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.print("Input length of row:");
-        String input=new Scanner(System.in).nextLine();
-        int length=Integer.parseInt(input);
-        if (length<=0){
-            System.out.println("0");
-            return;
-        }
-        System.out.print("Input max square of number: ");
-        input=new Scanner(System.in).nextLine();
-        int maxSqr= Integer.parseInt(input);
-        if (maxSqr<=0){
-            System.out.println("0");
-            return;
-        }
-        int base= (int) Math.sqrt(maxSqr)/1;
-        for (int i = 0; i < length; i++) {
-            System.out.print((int)Math.pow(++base,2));
-            if (i<length-1){
-                System.out.print(", ");
-            }
-        }
-//        System.out.println(base);
-
+        String lengthSeq=new Scanner(System.in).nextLine();
+	    System.out.print("Input max square of number: ");
+	    String maxSquare=new Scanner(System.in).nextLine();
+	    DigitSeq digitSeq=new DigitSeq();
+	    System.out.println(digitSeq.printSeq(lengthSeq,maxSquare));
     }
 }
